7536f519d7efe45c327c4b13874b5c9eaca110f9
liwenguang <1254755805@qq.com>
2016-12-16 05:01:07 +0800

    coolweather 的MyApp初始化问题

--- coolweather/src/main/java/com/example/coolweather/app/MyApp.java
+++ coolweather/src/main/java/com/example/coolweather/app/MyApp.java
@@ -1,17 +1,19 @@
 package com.example.coolweather.app;
 
+import android.app.Application;
+
 import org.litepal.LitePalApplication;
 
 /**
  * Created by li-pc on 2016/12/12.
  */
 
-public class MyApp extends LitePalApplication {
+public class MyApp extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-
+        LitePalApplication.initialize(this);
         //在这里为应用设置异常处理程序，然后我们的程序才能捕获未处理的异常
         CrashHandler crashHandler = CrashHandler.getInstance();
         crashHandler.init(this);
